9b2904bdf87bba70efb50270b841a31515fa954b
zead-a-g <101098246+zead-a-g@users.noreply.github.com>
2022-03-22 00:27:20 +0200

    22.03.22

--- action.js
+++ action.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const { status } = require('express/lib/response');
+const res = require('express/lib/response');
 const router = express.Router();
+
+
+
+let user =[];
+router.post('/register',(req,res) => {
+    
+    const {email,password} = req.body;
+
+    const data=user.filter(x=>x.email=='zead')
+
+    user.push({email:email,password:password});
+
+    return res.status(200).json({
+        message:user        
+    });
+})
 
 router.get('/greeting', (req,res) => {
 
